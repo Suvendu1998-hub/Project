a1f459df2824195c94c8d1e3e7e8bae07dffab0c
SuvenduMishra <63334558+Suvendu1998-hub@users.noreply.github.com>
2024-02-04 11:25:23 +0530

    Update hello.sh
    Hello Team I modified the code kindly check it

--- hello.sh
+++ hello.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo "Hello and Good Morning"
 echo "Good Evening Friends"
+echo "Hello and Good Morning"
+echo"Welcome to the session"
+echo "Hello and Good Morning"
+echo"Welcome to the session"
+echo "Hello and Good Morning"
+echo"Welcome to the session"
